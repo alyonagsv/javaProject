84ceaacb9243cb2061035c1ecdd0238081f67d2d
Alyona Guseva <elena347914@gmail.com>
2025-05-20 21:44:39 +0300

    Update ConversionResult.java

--- src/main/java/bsu/ConversionResult.java
+++ src/main/java/bsu/ConversionResult.java
@@ -1,15 +1,47 @@
 package bsu;
 
 public class ConversionResult {
-    public String from;
-    public String to;
-    public double amount;
-    public double result;
+    private String from;
+    private String to;
+    private double amount;
+    private double result;
 
     public ConversionResult(String from, String to, double amount, double result) {
         this.from = from;
         this.to = to;
         this.amount = amount;
+        this.result = result;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public double getResult() {
+        return result;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public void setResult(double result) {
         this.result = result;
     }
 }
